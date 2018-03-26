f0d927fdd5d0c151e6dd8cb3721af568f3fa9306
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2018-03-26 14:21:33 -0500

    CRA frame.

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
+import 'semantic-ui-css/semantic.min.css';
+
+render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('root'));
